c3f1c5c8aad1419cd90ce5c764ba638a3754acd8
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-12-12 16:45:31 -0800

    create react skeleton and add theme colors

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css';
+
+export default function Home() {
+    return (
+        <>
+            <div className="card">
+                <h1>Web Accessibility Quiz</h1>
+                <p>This quiz covers some of the fundamentals of the W3C Web Content Accessibility Guidelines (WCAG)</p>
+                <button>START QUIZ</button>
+            </div>
+        </>
+    )
+    
+}
