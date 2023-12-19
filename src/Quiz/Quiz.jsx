5f48d1dde1ad4d36869341caca6de727944792e4
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-12-19 12:20:02 -0800

    add counter for question page

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,15 +1,25 @@
 import './Quiz.css'
 import useDocumentTitle from '../useDocumentTitle'
 import questions from '../questions.json'
+import { useState } from 'react'
 
 export default function Quiz() {
-    useDocumentTitle('Accessibility Quiz - Question 1')
+    const [count, setCount] = useState(0)
+
+    useDocumentTitle(`Question ${count+1} - Accessibility Quiz`)
 
     return (
         <>
             <div className="card">
-                <h1>Question #1</h1>
-                <p>{questions[0].question}</p>
+                <h1>Question #{count+1}</h1>
+                <p>{questions[count].question}</p>
+                {count < 9 ? (
+                    <button onClick={() => setCount(count+1)}>Next</button>
+                ) : (
+                    <button>Submit</button>
+                )
+                }
+
             </div>
         </>
     )
